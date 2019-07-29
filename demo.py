3f874614d4c86903ea766be0a271b22e5742ff3c
Camille Taylor <camilletaylor@Camilles-MacBook-Air.local>
2019-07-29 10:27:57 -0700

    Added demo.py

---
+++ demo.py
@@ -0,0 +1,15 @@
+# -------------------------------------------------
+#        Name: Camille 
+#    Filename: .py
+#        Date: July , 2019
+#
+# Description: sample python file
+# -------------------------------------------------
+
+def main():
+    
+    name = "Camille"
+    print("Hello my name is", name)
+
+if __name__ = "__main__":
+    main()
